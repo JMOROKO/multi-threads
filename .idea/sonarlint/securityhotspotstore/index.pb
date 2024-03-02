2b147a346b8b0b0fc85e0c9d5787f935e7b50698
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2024-03-02 16:07:29 +0000

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+B
+src/Talkative.java,e\9\e9e4a6a64599fa2eb966aacabdf0b8451e207976
---
+++ src/Talkative.java
@@ -0,0 +1,15 @@
+public class Talkative implements Runnable{
+    private int attr;
+
+    public Talkative(int attr) {
+        this.attr = attr;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++){
+            attr++;
+            System.out.println(attr);
+        }
+    }
+}
